26887c9ba8173df0923654f03866fbbd2488e680
Irina Serysheva <irina.serysheva@fabrique.studio>
2023-05-19 23:22:11 +0800

    Закончен футер

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.scss';
 import Button from './components/Button/Button';
-import { ReactComponent as chevronLeft } from './assets/icons/chevron-left.svg';
+import chevronLeft from './assets/icons/chevron-left.svg';
+import man from './assets/icons/man.svg';
 
 function App() {
   return (
     <div className="app">
       <main className="app__main">main</main>
       <footer className="app__footer">
-        <Button>Вызов сотрудника</Button>
+        <Button type="square" icon={chevronLeft} />
+        <Button icon={man}>Вызов сотрудника</Button>
       </footer>
     </div>
   );
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,5 @@
 import styles from './Button.module.scss';
+import { ReactSVG } from 'react-svg';
 
 const Types = {
   Default: 'default',
@@ -11,7 +12,12 @@ function Button(props) {
     props.type === Types.Square ? styles.square : '',
   ];
 
-  return <button className={buttonClasses.join(' ')}>{props.children}</button>;
+  return (
+    <button className={buttonClasses.join(' ')}>
+      {props.icon ? <ReactSVG src={props.icon} className={styles.icon} /> : ''}
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
